379ad760ec01b7367c3fdf8662faa8cff1ca6164
Igor Ponomarov <absvox@gmail.com>
2021-04-24 20:00:28 +0200

    Made function get_parties_votes in main modul

--- election_scraper.py
+++ election_scraper.py
@@ -16,6 +16,7 @@ def get_html(url):
     r = requests.get(url)
     return r.text
 
+
 def get_all_links(html):
     soup = bs(html, 'html.parser')
     divs = soup.find_all('div', class_='t3')
@@ -30,6 +31,31 @@ def get_all_links(html):
             links_tab.append(link)
         links_all.extend(links_tab)
     return links_all
+
+
+# Vystupem je list tuplu, obsahujici pary cisel - poradkove cislo strany a pocet platnych hlasu
+def get_parties_votes(url):
+    r = requests.get(URL)
+    soup = bs(r.text, 'html.parser')
+
+    divs = soup.find_all('div', class_='t2_470')
+    # Vytvorime prazdnyj list pro poradkova cisla a pocet hlasu vsech stran, kandidujicich v obci
+    parties_all = []
+    for i, div in enumerate(divs, start=1):
+        headers_nums = f't{i}sa1 t{i}sb1'
+        headers_votes = f't{i}sa2 t{i}sb3'
+        parties_nums = div.find_all('td', class_='cislo', headers=headers_nums)
+        parties_votes = div.find_all('td', class_='cislo', headers=headers_votes)
+        parties_div = []
+        for n in range(len(parties_nums)):
+            parties_div.append((parties_nums[n].text, parties_votes[n].text))
+        parties_all.extend(parties_div)
+
+    return parties_all
+
+
+
+
 
 def main():
     html = get_html(URL)
